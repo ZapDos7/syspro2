b727cbd87e2d1424045b88653143462f4fdfbf88
ZapDos7 <ioanna.zapalidi@gmail.com>
2020-05-20 20:34:15 +0300

    fixing

--- record.cpp
+++ record.cpp
@@ -24,6 +24,7 @@ record::record(string line) //thewrw oti dineis sxetika swsto record kai xeirizo
         counter++;
         pch = strtok(NULL, delim);
     }
+    std::cerr << counter << "\n";
     for (unsigned int i = 0; i < 8; i++)
     {
         string tmp(parts[i]);
